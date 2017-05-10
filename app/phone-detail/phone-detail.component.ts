4dc785e5d62b9ed66a595dc231ec424d01e6ef90
rockncoder <rockncoder@gmail.com>
2017-05-10 17:15:04 -0500

    Adds systemjs.config.js and angular in the package.json

--- app/phone-detail/phone-detail.component.ts
+++ app/phone-detail/phone-detail.component.ts
@@ -1,20 +1,20 @@
-'use strict';
-
-// Register `phoneDetail` component, along with its associated controller and template
+class PhoneDetailController {
+  phone: any;
+  mainImageUrl: string;
+  static $inject = ['$routeParams', 'Phone'];
+  constructor($routeParams: angular.route.IRouteParamsService, Phone: any) {
+    let phoneId = $routeParams['phoneId'];
+    this.phone = Phone.get({phoneId}, (phone: any) => {
+      this.setImage(phone.images[0]);
+    });
+  }
+  setImage(imageUrl: string) {
+    this.mainImageUrl = imageUrl;
+  }
+}
 angular.
-  module('phoneDetail').
-  component('phoneDetail', {
-    templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams, Phone) {
-        var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-          self.setImage(phone.images[0]);
-        });
-
-        self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
-  });
+module('phoneDetail').
+component('phoneDetail', {
+  templateUrl: 'phone-detail/phone-detail.template.html',
+  controller: PhoneDetailController
+});
