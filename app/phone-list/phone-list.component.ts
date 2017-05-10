e125b230b2f7213a0ff3f80f8d994d804ab51ae2
rockncoder <rockncoder@gmail.com>
2017-05-10 02:40:56 -0500

    Changes phone-list to a class

--- app/phone-list/phone-list.component.ts
+++ app/phone-list/phone-list.component.ts
@@ -1,14 +1,15 @@
-'use strict';
+class PhoneListController {
+  phones: any[];
+  orderProp: string;
+  query: string;
+  static $inject = ['Phone'];
 
-// Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
-  });
+  constructor(Phone: any) {
+    this.phones = Phone.query();
+    this.orderProp = 'age';
+  }
+}
+angular.module('phoneList').component('phoneList', {
+  templateUrl: 'phone-list/phone-list.template.html',
+  controller: PhoneListController
+});
