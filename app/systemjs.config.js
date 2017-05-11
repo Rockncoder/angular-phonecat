b43b205fcb6e0bdee750e0abdb2328903a257ddc
rockncoder <rockncoder@gmail.com>
2017-05-10 21:38:18 -0500

    Adds system.js stuff,but now have an error

--- app/systemjs.config.js
+++ app/systemjs.config.js
@@ -5,13 +5,14 @@
 (function (global) {
   System.config({
     paths: {
+
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm:': '/node_modules/'
     },
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      'app': 'app',
+      app: '/app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -22,6 +23,7 @@
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
 
       // other libraries
       'rxjs':                      'npm:rxjs',
